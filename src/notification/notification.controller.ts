0db155a2b03fd709a4b3361d45b8460826ea975a
Nofaldi Fikrul Atmam <nsiren517@gmail.com>
2022-05-26 20:01:44 +0700

    feat: rpc notif (#2)
    Co-authored-by: nofamex <nofaldi.atmam@tiket.com>

---
+++ src/notification/notification.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Inject,
+  OnModuleInit,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Observable } from 'rxjs';
+import { AuthGuard } from 'src/auth/auth.guard';
+import {
+  ListRequest,
+  ListResponse,
+  NotificationServiceClient,
+  NOTIFICATION_SERVICE_NAME,
+} from './notification.pb';
+
+@ApiTags('notification')
+@Controller('notification')
+export class NotificationController implements OnModuleInit {
+  private svc: NotificationServiceClient;
+
+  @Inject(NOTIFICATION_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<NotificationServiceClient>(
+      NOTIFICATION_SERVICE_NAME,
+    );
+  }
+
+  @Post('list')
+  @ApiBearerAuth('access-token')
+  @UseGuards(AuthGuard)
+  @ApiResponse({ status: 201, description: 'list of user notification' })
+  private async list(@Req() req: any): Promise<Observable<ListResponse>> {
+    const body: ListRequest = { userId: req.user };
+    return this.svc.list(body);
+  }
+}
