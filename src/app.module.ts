c454582a32c2994dffb12b153311226fe1c6352c
nofamex <nofaldi.atmam@tiket.com>
2022-05-27 16:20:51 +0700

    fix: product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notification/notification.module';
+import { ProductModule } from './product/product.module';
 import { PromoModule } from './promo/promo.module';
 
 @Module({
-  imports: [AuthModule, NotificationModule, PromoModule],
+  imports: [AuthModule, NotificationModule, PromoModule, ProductModule],
 })
 export class AppModule {}
