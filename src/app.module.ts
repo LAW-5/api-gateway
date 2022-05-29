4b175e565fcc81e03ce52dd9a70873ff6a66a6f3
Annisa Dian <nugrahani07@gmail.com>
2022-05-29 14:40:04 +0700

    feat: order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notification/notification.module';
+import { OrderModule } from './order/order.module';
 import { PromoModule } from './promo/promo.module';
 
 @Module({
-  imports: [AuthModule, NotificationModule, PromoModule],
+  imports: [AuthModule, NotificationModule, PromoModule, OrderModule],
 })
 export class AppModule {}
---
+++ src/order/order.dto.ts
@@ -0,0 +1,62 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateOrderDto {
+  @ApiProperty()
+  merchantId: number;
+
+  @ApiProperty()
+  productId: number;
+
+  @ApiProperty()
+  quantity: number;
+
+  @ApiProperty()
+  address: string;
+
+  @ApiProperty()
+  promoId: number[];
+
+  @ApiProperty()
+  orderStatus: string;
+}
+
+export class CreateOrderPayload {
+  @ApiProperty()
+  userId: number;
+
+  @ApiProperty()
+  merchantId: number;
+
+  @ApiProperty()
+  productId: number;
+
+  @ApiProperty()
+  quantity: number;
+
+  @ApiProperty()
+  address: string;
+
+  @ApiProperty()
+  promoId: number[];
+
+  @ApiProperty()
+  orderStatus: string;
+}
+
+export class ListOrderDto {
+  @ApiProperty()
+  userId: number;
+}
+
+export class ListMerchantOrderDto {
+  @ApiProperty()
+  merchantId: number;
+}
+
+export class EditOrderDto {
+  @ApiProperty()
+  id: number;
+  
+  @ApiProperty()
+  orderStatus: string;
+}
