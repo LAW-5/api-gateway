a3c2987b89db601eeb526080cc76fda672af8075
Annisa Dian <nugrahani07@gmail.com>
2022-05-30 16:32:14 +0700

    feat: order

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notification/notification.module';
 import { ProductModule } from './product/product.module';
 import { PromoModule } from './promo/promo.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [AuthModule, NotificationModule, PromoModule, ProductModule],
+  imports: [AuthModule, NotificationModule, PromoModule, ProductModule, OrderModule],
 })
+
 export class AppModule {}
